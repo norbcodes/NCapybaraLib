5af287fc158fee3dfc2a08499cb7183ec30e3b12
norbcodes <thenorbert2008@gmail.com>
2024-04-19 20:01:47 +0200

    Added 2 new functions!!

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 <h1>NCapybaraLib</h1>
 <p>A small library with a bunch of functions and thingies I made for fun.<br>So far, there's nothing much, but it will grow as time goes.</p>
+<p>Check out the library on <a href="https://pypi.org/project/NCapybaraLib/">PyPi</a>!</p>
 
 <h1>Download</h1>
 <p>To download this library, run this command:</p>
-<code>pip install NCapybaraLib</code>
+<blockquote>pip install NCapybaraLib</blockquote>
--- src/NCapybaraLib/__init__.py
+++ src/NCapybaraLib/__init__.py
@@ -1,2 +1,31 @@
+"""
+NCapybaraLib
+
+A small library with a bunch of functions and thingies I made for fun.
+
+https://pypi.org/project/NCapybaraLib/
+"""
+
+from typing import Callable, Any
+
 def print_hello_world() -> None:
+    """
+    Hello world program straight from NCapybaraLib itself.
+    """
     print("Hello world!")
+
+def inject_function(obj: Any, func_name: str, func: Callable[[Any], Any]) -> Any:
+    """
+    Does exactly what it sounds like. Injects *func* as a method for *obj* under the name *func_name*.
+
+    :param obj: The object instance name referring to the object to inject into.
+    :param func_name: The method name.
+    :param func: The function to inject.
+    :return:
+    """
+    scope = {"target": obj, "injection": func}
+    try:
+        exec(f"target.{func_name}=classmethod(injection).__get__(None, target)", scope)
+    except AttributeError as A:
+        raise AttributeError(f"Cannot inject function {func.__name__} as {func_name} into object of type {type(obj)}.") from A
+    return scope["target"]
